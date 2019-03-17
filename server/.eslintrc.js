3299823e12839145bd9deff6263f26eaa569868b
Emil Niyaz <niyaz.emil@gmail.com>
2019-03-17 01:47:36 -0400

    setup express js server

---
+++ server/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        'no-console': 'off',
+        "no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }]
+    }
+};
